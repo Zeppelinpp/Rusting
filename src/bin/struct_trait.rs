34cae93a3b1dc9cb1ef81f56db612c06d712b386
Zeppelinpp <1056118649@qq.com>
2026-02-20 23:21:33 +0800

    learn trait
    - Compostion over inheritance
    - match with reference won't move ownership (safe)

---
+++ src/bin/struct_trait.rs
@@ -0,0 +1,98 @@
+use std::any::type_name;
+
+#[derive(Debug)]
+enum Gender {
+    Male,
+    Female,
+    DontCare(String),
+}
+
+#[derive(Debug)]
+struct BasicInfo {
+    name: String,
+    age: i8,
+    gender: Gender,
+}
+
+struct IndieMusician {
+    info: BasicInfo,
+    personality: String,
+}
+
+struct PopSinger {
+    info: BasicInfo,
+}
+
+trait ReleaseAlbum {
+    fn get_info(&self) -> &BasicInfo;
+
+    fn compose(&self) {
+        let yourself = type_name::<Self>();
+        let short_name = yourself.split("::").last().unwrap_or(yourself);
+        println!("--- {} composing ... ---", short_name);
+
+        let info = self.get_info();
+        println!(
+            "Name: {}\nAge: {}\nGender: {}",
+            info.name,
+            info.age,
+            match &info.gender {
+                Gender::Male => "Male",
+                Gender::Female => "Female",
+                Gender::DontCare(desc) => desc,
+            }
+        );
+        self.apply_personality();
+    }
+
+    fn apply_personality(&self);
+}
+
+impl ReleaseAlbum for IndieMusician {
+    fn get_info(&self) -> &BasicInfo {
+        &self.info
+    }
+    fn apply_personality(&self) {
+        println!("Applying {}...", self.personality);
+        println!("--- Finish ---\n");
+    }
+}
+
+impl ReleaseAlbum for PopSinger {
+    fn get_info(&self) -> &BasicInfo {
+        &self.info
+    }
+    fn apply_personality(&self) {
+        println!("PopSinger have no personality, skip it. Making good money");
+        println!("--- Finish ---\n");
+    }
+}
+
+fn main() {
+    let indie_musician = IndieMusician {
+        info: BasicInfo {
+            name: String::from("Tame Impala"),
+            age: 30,
+            gender: Gender::DontCare(String::from("maybe gay")),
+        },
+        personality: String::from("Indie/Neo-Psychedelia/Psychedelic Pop"),
+    };
+    let pop_singer_1 = PopSinger {
+        info: BasicInfo {
+            name: String::from("Taylor Swift"),
+            age: 32,
+            gender: Gender::Female,
+        },
+    };
+    let pop_singer_2 = PopSinger {
+        info: BasicInfo {
+            name: String::from("Drake"),
+            age: 36,
+            gender: Gender::Male,
+        },
+    };
+
+    indie_musician.compose();
+    pop_singer_1.compose();
+    pop_singer_2.compose();
+}
